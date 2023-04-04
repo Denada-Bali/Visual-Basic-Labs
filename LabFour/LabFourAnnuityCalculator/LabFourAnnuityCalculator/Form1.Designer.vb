9f959da331997e77f49b94201dce4d9cb447f570
Denalda-Bali <denaldabali@gmail.com>
2023-04-05 01:25:41 +0200

    ANNUITY CALCULATOR

--- LabFour/LabFourAnnuityCalculator/LabFourAnnuityCalculator/Form1.Designer.vb
+++ LabFour/LabFourAnnuityCalculator/LabFourAnnuityCalculator/Form1.Designer.vb
@@ -22,10 +22,174 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        RadioButton4 = New RadioButton()
+        RadioButton5 = New RadioButton()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        GroupBox1 = New GroupBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(37, 41)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(95, 19)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Present Value"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(37, 70)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(147, 19)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Interest Rate per period"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(37, 101)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(138, 19)
+        RadioButton3.TabIndex = 2
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Number of payments"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.Location = New Point(37, 130)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(118, 19)
+        RadioButton4.TabIndex = 3
+        RadioButton4.TabStop = True
+        RadioButton4.Text = "Periodic Peyment"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton5
+        ' 
+        RadioButton5.AutoSize = True
+        RadioButton5.Location = New Point(37, 159)
+        RadioButton5.Name = "RadioButton5"
+        RadioButton5.Size = New Size(90, 19)
+        RadioButton5.TabIndex = 4
+        RadioButton5.TabStop = True
+        RadioButton5.Text = "Future value"
+        RadioButton5.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(209, 37)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 5
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(209, 66)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 6
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(209, 97)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 7
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(209, 126)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 8
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(209, 155)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(100, 23)
+        TextBox5.TabIndex = 9
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Controls.Add(TextBox5)
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Controls.Add(TextBox4)
+        GroupBox1.Controls.Add(RadioButton2)
+        GroupBox1.Controls.Add(TextBox3)
+        GroupBox1.Controls.Add(RadioButton3)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(RadioButton4)
+        GroupBox1.Controls.Add(RadioButton5)
+        GroupBox1.Location = New Point(27, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(335, 208)
+        GroupBox1.TabIndex = 10
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Check Value to Calculate"' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(84, 236)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(212, 23)
+        Button1.TabIndex = 11
+        Button1.Text = "Calculate Value"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(84, 274)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(212, 23)
+        Button2.TabIndex = 12
+        Button2.Text = "Clear All Values"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(392, 323)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Annuity"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
     End Sub
 
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- LabFour/LabFourAnnuityCalculator/LabFourAnnuityCalculator/Form1.vb
+++ LabFour/LabFourAnnuityCalculator/LabFourAnnuityCalculator/Form1.vb
@@ -1,3 +1,102 @@
-﻿Public Class Form1
+﻿Imports System.Windows.Forms.VisualStyles
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Button
+
+Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox5.Clear()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim presentValue As Double
+        Dim ratePerPeriod As Double
+        Dim numberOfPayments As Integer
+        Dim periodicPayment As Double
+        Dim futureValue As Double
+
+        If (ValidateInput() = False) Then
+            Exit Sub
+        End If
+
+        If RadioButton1.Checked Then
+            ratePerPeriod = CDbl(TextBox2.Text)
+            numberOfPayments = CDbl(TextBox3.Text)
+            periodicPayment = CDbl(TextBox4.Text)
+            futureValue = CDbl(TextBox5.Text)
+            TextBox1.Text = Financial.PV(ratePerPeriod, numberOfPayments, periodicPayment, -futureValue).ToString("C2")
+
+        ElseIf RadioButton2.Checked Then
+            presentValue = CDbl(TextBox1.Text)
+            numberOfPayments = CDbl(TextBox3.Text)
+            periodicPayment = CDbl(TextBox4.Text)
+            futureValue = CDbl(TextBox5.Text)
+            TextBox2.Text = (Financial.Rate(numberOfPayments, -periodicPayment, -presentValue, futureValue)).ToString("N5")
+
+        ElseIf RadioButton3.Checked Then
+            presentValue = CDbl(TextBox1.Text)
+            ratePerPeriod = CDbl(TextBox2.Text)
+            periodicPayment = CDbl(TextBox4.Text)
+            futureValue = CDbl(TextBox5.Text)
+            TextBox3.Text = Financial.NPer(ratePerPeriod, -periodicPayment, -presentValue, futureValue).ToString("N0")
+
+        ElseIf RadioButton4.Checked Then
+            presentValue = CDbl(TextBox1.Text)
+            ratePerPeriod = CDbl(TextBox2.Text)
+            numberOfPayments = CDbl(TextBox3.Text)
+            futureValue = CDbl(TextBox5.Text)
+            TextBox4.Text = (Financial.Pmt(ratePerPeriod, numberOfPayments, -presentValue, futureValue) * -1).ToString("N2")
+
+        ElseIf RadioButton5.Checked Then
+            presentValue = CDbl(TextBox1.Text)
+            ratePerPeriod = CDbl(TextBox2.Text)
+            numberOfPayments = CDbl(TextBox3.Text)
+            periodicPayment = CDbl(TextBox4.Text)
+            TextBox5.Text = Financial.FV(ratePerPeriod, numberOfPayments, -periodicPayment, -presentValue).ToString("C2")
+
+        End If
+    End Sub
+
+    Private Function ValidateInput() As Boolean
+
+        If RadioButton1.Checked And IsNumeric(TextBox2.Text) _
+            And IsNumeric(TextBox3.Text) And IsNumeric(TextBox4.Text) _
+            And IsNumeric(TextBox1.Text) Then
+
+            Return True
+
+        ElseIf RadioButton2.Checked And IsNumeric(TextBox1.Text) _
+            And IsNumeric(TextBox3.Text) And IsNumeric(TextBox4.Text) _
+            And IsNumeric(TextBox1.Text) Then
+
+            Return True
+
+        ElseIf RadioButton3.Checked And IsNumeric(TextBox1.Text) _
+            And IsNumeric(TextBox2.Text) And IsNumeric(TextBox4.Text) _
+            And IsNumeric(TextBox1.Text) Then
+
+            Return True
+
+        ElseIf RadioButton4.Checked And IsNumeric(TextBox1.Text) _
+            And IsNumeric(TextBox2.Text) And IsNumeric(TextBox4.Text) _
+            And IsNumeric(TextBox1.Text) Then
+
+            Return True
+
+        ElseIf RadioButton5.Checked And IsNumeric(TextBox1.Text) _
+            And IsNumeric(TextBox2.Text) And IsNumeric(TextBox4.Text) _
+            And IsNumeric(TextBox3.Text) Then
+
+            Return True
+
+        End If
+
+        MessageBox.Show("Input Data does not meet the requirements!", "Information")
+        Return False
+    End Function
+
 
 End Class
